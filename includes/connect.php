afb0ea0f867f352249b35de63d76f999961e1f5d
Marwane Assou <marwane.assou@gmail.com>
2024-01-21 19:24:40 +0100

    includes folder

--- includes/connect.php
+++ includes/connect.php
@@ -2,3 +2,10 @@
 <?php
     $connect=mysqli_connect('localhost','your username','your password','rental project');
     // don't forget to enter your username and password of database management system which you use and keep the name of database 'rental project'
+
+
+
+
+
+
+    
